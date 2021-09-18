9389604adb35548e6cfa4b51ac70734e7b2b95ad
francescodesimone <f.desimone30@studenti.unisa.it>
2021-09-18 13:42:36 +0200

    Modify config

---
+++ build_appimage.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -xeu
+
+clean_up(){
+    rm -rf ./AppDir/usr/bin
+    rm -rf ./AppDir/node
+    rm -rf ./AppDir/python
+    rm -fr ./AppDir/SpaceVim.d
+    rm -fr ./AppDir/SpaceVim
+    rm -fr ./AppDir/rust
+}
+
+clone_spacevim(){
+    git clone https://github.com/SpaceVim/SpaceVim.git AppDir/SpaceVim
+    mkdir -p ./AppDir/SpaceVim.d
+    cp ./init.toml ./AppDir/SpaceVim.d/init.toml
+    cp -r ./autoload ./AppDir/SpaceVim.d/autoload
+}
+
+copy_binary(){
+    mkdir ./AppDir/usr/bin
+    find ./AppDir/usr -type f -exec cp "{}" ./AppDir/usr/bin \;
+}
+
+install_python_dep(){
+    mkdir -p ./AppDir/python
+    PIP_TARGET="./AppDir/python" pip install --target="./AppDir/python" \
+        python-language-server yapf isort coverage autoflake pylint 
+    cp ./AppDir/python/bin/* ./AppDir/usr/bin
+    rm -rf ./AppDir/python
+}
+
+install_node_dep(){
+    mkdir -p ./AppDir/node
+    npm install -g --prefix ./AppDir/node \
+                        javascript-typescript-langserver \
+                        vscode-css-languageserver-bin \
+                        bash-language-server \
+                        purescript-language-server
+    
+    cp ./AppDir/node/bin/* ./AppDir/usr/bin
+    rm -rf ./AppDir/node
+}
+
+install_rust_dep(){
+    mkdir ./AppDir/rust
+    RUSTUP_TOOLCHAIN="stable" RUSTUP_HOME="./AppDir/rust" CARGO_HOME="./AppDir/rust" rustup component add rls rust-analysis rust-src 
+    cp ./AppDir/rust/toolchains/stable-x86_64-unknown-linux-gnu/bin/* ./AppDir/usr/bin
+    rm -rf ./AppDir/rust
+}
+
+
+clean_up
+clone_spacevim
+copy_binary
+install_python_dep
+install_node_dep
+install_rust_dep
+appimagetool AppDir
+clean_up
